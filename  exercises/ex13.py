406d03d15ebf4bea05afd92a6f339fa0fbee5782
lipeng <marsapolo@163.com>
2017-12-05 10:56:47 +0800

    创建练习文件夹

---
+++  exercises/ex13.py
@@ -0,0 +1,54 @@
+# _*_ coding:utf-8 _*_
+from sys import argv
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+age = raw_input('prompt')
+print age
+# age = raw_input("How old are you? ")
+# height = raw_input("How tall are you? ")
+# weight = raw_input("How much do you weight? ")
+
+# print "So, you're %r old, %r tall and %r heavy." % (age,height,weight)
+
+# n = 0;
+# while True :
+# 	for i in ["/","-","\\","|"]:
+# 		print "%s\r" % i,
+# print "Mary had a little lamb. "
+# print "Its fleece was white as %s. " % '雪'
+# print "And everywhere that Mary went. "
+# print "." * 10 #what'd that do?
+
+# end1 = "C"
+# end2 = "h"
+# end3 = "e"
+# end4 = "e"
+# end5 = "s"
+# end6 = "e"
+# end7 = "B"
+# end8 = "u"
+# end9 = "r"
+# end10 = "g"
+# end11 = "e"
+# end12 = "r"
+
+#watch that comma at the end. try removing it to see what happens
+# print end1 + end2 + end3 + end4 + end5 + end6,
+# print end7 + end8 + end9 + end10 + end11 +end12
+
+# print """
+# There's somthing going on here.
+#         With the three double-quotes.
+# 1
+# 	2
+# 		3
+# 			4
+# 				5
+# 					6
+# 						7
+# 							8
+# """
